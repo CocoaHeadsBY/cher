ce7832eb785d97495aca372780fe846b07269484
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-11-08 16:15:41 +0300

    WIP for Cyrill with compile errors

--- CherExtention/ShareViewController.swift
+++ CherExtention/ShareViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 import MobileCoreServices
 
-class ShareViewController: UIViewController {
+enum Result<T> {
+    case Success(T)
+    case Failure(String)
+    func process<U>(s: T -> U, f: String -> U) -> U {
+        switch (self) {
+        case let .Success(val):
+            return s(val)
+        case let .Failure(err):
+            return f(err)
+        }
+    }
+}
 
-    func finish() {
-        self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+class ShareViewController: UIViewController {
+    let s = { (r: String) -> () in
+
+        //self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+    }
+
+    let f = { (e: String) -> () in
+        print("Error:")
+    }
+
+    func finish(result: Result<String>) {
+        result.process(s, f)
+
     }
 
     // MARK: NSExtensionRequestHandling
@@ -26,18 +48,20 @@ class ShareViewController: UIViewController {
                 let itemProvider = provider as NSItemProvider
                 if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as NSString) {
                     itemProvider.loadItemForTypeIdentifier(kUTTypeImage as NSString, options: nil, completionHandler: { (image, error) in
-                        if image != nil {
-                            NSOperationQueue.mainQueue().addOperationWithBlock {
-                                println(image as? NSURL)
+                        return ()
+//                        (image as? NSURL).map { (imageURL : NSURL) -> () in
+//                            NSOperationQueue.mainQueue().addOperationWithBlock {
+//                                println(imageURL)
+//                                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), { () -> Void in
+//                                    var result = Result.Success(image)
+//                                    self.finish()
+//                                })
+//                            }
+//                        }
 
-                                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), { () -> Void in
-                                    self.finish()
-                                })
-                            }
-                        }
                     })
 
-                    break
+                    //break
                 }
             }
         }
